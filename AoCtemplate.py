6d34057a8f3686f01f121183672e0382febe5cec
Gerwin Osnabrugge <g.osnabrugge@utwente.nl>
2024-12-07 11:08:29 +0100

    Added AoCtemplate script

---
+++ AoCtemplate.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Advent of Code day .. script
+author: g.osnabrugge
+"""
+#import numpy as np
+import time
+
+#%%# helper functions #%%#
+
+#%%# readout puzzle input file #%%#
+# # extract all contents of input file
+# inputs = open('input.txt').read()
+
+# # extract entrees per puzzle line
+# for line in open('input.txt'):
+#     entrees = list(map(int,line.split(' ')))
+
+# # readout puzzle txt file into separate string lines (without white spaces)
+# with open('input.txt') as file:
+#     lines = [line.rstrip() for line in file]
+
+#%%%# Part 1 #%%#
+t0 = time.time()
+resultsPart1 = 0
+
+
+#%%%# Part 2 #%%#
+resultsPart2 = 0
+
+#%%%# print results #%%#
+tTot = time.time()-t0
+print('Part 1: '+ str(resultsPart1))
+print('Part 2: '+ str(resultsPart2))
+print(f'runtime: {tTot:.3f} sec')
