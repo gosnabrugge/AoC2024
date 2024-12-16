b1021ae4ca30830760273aa56c4808480ee9ca1c
Osnabrugge <g.osnabrugge@phix.com>
2024-12-16 10:51:55 +0100

    finally solved day12 (spaghetti version)

--- day12/AoCday12.py
+++ day12/AoCday12.py
@@ -14,72 +14,103 @@ def loadData(inputFile):
         return [list(line.rstrip()) for line in file]
 
 def fence(grid,pos):
+    ## find all plants in region
     pType = grid[pos[0]][pos[1]]
     plantsInRegion = [[pos[0],pos[1]]]
-    area = 1
-    perimeter = 0
-    nSides = 0
-    grid[pos[0]][pos[1]] = '.'
-    fencesSet = [[False]*4] # keeps track which fences have already been started before
     for i, plant in enumerate(plantsInRegion):
         row = plant[0]
         col = plant[1]
-        newPlants = 0
+    
+        if row > 0 and grid[row-1][col] == pType and not [row-1,col] in plantsInRegion: 
+            plantsInRegion.append([row-1,col]) # add upper neighbour
+        if col < len(grid[0])-1 and grid[row][col+1] == pType and not [row,col+1] in plantsInRegion: 
+            plantsInRegion.append([row,col+1]) # add right neighbour
+        if row < len(grid)-1 and grid[row+1][col] == pType and not [row+1,col] in plantsInRegion:
+            plantsInRegion.append([row+1,col]) # add lower neighbour
+        if col > 0 and grid[row][col-1] == pType and not [row,col-1] in plantsInRegion: 
+            plantsInRegion.append([row,col-1]) # add left neighbour
+    
+    area = len(plantsInRegion) 
+    
+    # check total size of region
+    rows = [plant[0] for plant in plantsInRegion]
+    cols = [plant[1] for plant in plantsInRegion]
+    
+    ## find perimeter and number of sides of region
+    nSides = 0
+    perimeter = 0
+    
+    # find all vertical sides of region (checks all rows for edges)
+    startEdges = []
+    endEdges = []
+    inRegion = False
+    for i in range(min(rows),max(rows)+1):
+        # list to track which sides have been found before
+        prevStartEdges = startEdges
+        prevEndEdges = endEdges
+        startEdges = []
+        endEdges = []
         
-        # first check for borders if fences are needed      
-        fences = [False]*4
-        if not [row-1,col] in plantsInRegion: # check up
-            if row > 0 and grid[row-1][col] == pType:
-                plantsInRegion.append([row-1,col])
-                grid[row-1][col] = '.'
-                area+=1
-                newPlants += 1
-            else: 
-                perimeter += 1
-                fences[0] = True
-                if not fencesSet[i][0]: nSides += 1
-        if not [row,col+1] in plantsInRegion: # check right
-            if col < len(grid[0])-1 and grid[row][col+1] == pType:
-                plantsInRegion.append([row,col+1])
-                grid[row][col+1] = '.'
-                area+=1
-                newPlants += 1
-            else: 
-                perimeter += 1
-                fences[1] = True
-                if not fencesSet[i][1]: nSides += 1
-        if not [row+1,col] in plantsInRegion: # check down
-            if row < len(grid)-1 and grid[row+1][col] == pType:
-                plantsInRegion.append([row+1,col])
-                grid[row+1][col] = '.'
-                area+=1
-                newPlants += 1
-            else: 
-                perimeter += 1
-                fences[2] = True
-                if not fencesSet[i][2]: nSides += 1
-        if not [row,col-1] in plantsInRegion: # check left
-            if col > 0 and grid[row][col-1] == pType:
-                plantsInRegion.append([row,col-1])
-                grid[row][col-1] = '.'
-                area+=1
-                newPlants += 1
-            else: 
-                perimeter += 1
-                fences[3] = True
-                if not fencesSet[i][3]: nSides += 1
-        
-        # add fences to set
-        for iPlant in range(newPlants): fencesSet.append(fences)
+        for j in range(min(cols),max(cols)+1):
+            if [i,j] in plantsInRegion and not inRegion:
+                startEdges.append(j)
+                inRegion = True
+            elif not [i,j] in plantsInRegion and inRegion:
+                endEdges.append(j)
+                inRegion = False
             
+            if j == max(cols) and inRegion: # if last space was in region then end with fence
+                endEdges.append(j+1)
+                inRegion = False
         
+        # determine perimeter and number of sides
+        perimeter += len(startEdges) + len(endEdges)
+        for edge in startEdges: 
+            if not edge in prevStartEdges: nSides += 1
+        for edge in endEdges: 
+            if not edge in prevEndEdges: nSides += 1
+        
+    # find all horizontal sides of region (checks all columns for edges)
+    startEdges = []
+    endEdges = []
+    inRegion = False
+    for j in range(min(cols),max(cols)+1):
+        # list to track which sides have been found before
+        prevStartEdges = startEdges
+        prevEndEdges = endEdges
+        startEdges = []
+        endEdges = []
+        for i in range(min(rows),max(rows)+1):
+            if [i,j] in plantsInRegion and not inRegion:
+                startEdges.append(i)
+                inRegion = True
+            elif not [i,j] in plantsInRegion and inRegion:
+                endEdges.append(i)
+                inRegion = False
+            
+            if i == max(rows) and inRegion: # if last space was in region then end with fence
+                endEdges.append(i+1)
+                inRegion = False
+        
+        # determine perimeter and number of sides
+        perimeter += len(startEdges) + len(endEdges)
+        for edge in startEdges: 
+            if not edge in prevStartEdges: nSides += 1
+        for edge in endEdges: 
+            if not edge in prevEndEdges: nSides += 1
+        
+        
+
+    # remove all plant spaces in region from grid
+    for plant in plantsInRegion: grid[plant[0]][plant[1]] = '.'
+    
     cost = area*perimeter
     cost2 = area*nSides
     return grid, cost, cost2
         
 
 #%%# readout puzzle input file #%%#
-garden = loadData('test.txt')
+garden = loadData('input.txt')
 nRows = len(garden)
 nCols = len(garden[0])
 
